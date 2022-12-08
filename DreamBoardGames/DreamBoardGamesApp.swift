812d7a3c6420e3779c21b84fdb294baecb8f142e
Ugo Mariotto <ugo.mariotto@etu.iut-tlse3.fr>
2022-12-08 13:37:43 +0100

    Initial Commit

---
+++ DreamBoardGames/DreamBoardGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamBoardGamesApp.swift
+//  DreamBoardGames
+//
+//  Created by Ugo Mariotto on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DreamBoardGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
